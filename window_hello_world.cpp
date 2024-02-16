5b0ea7ed4eb163143f222fbb8ebc26057fc9f625
Mehul-lunia <mehulrajlunia@gmail.com>
2024-02-16 08:57:10 +0530

    small change, added MB_ICONASTERISK

--- window_hello_world.cpp
+++ window_hello_world.cpp
@@ -3,5 +3,5 @@
 // Displays a Window showing the text Hello World!
 int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
 {
-    return MessageBox(NULL,"Hello World!","My first windowed program",0);
+    return MessageBox(NULL,"Hello World!","My first windowed program",MB_ICONASTERISK);
 }

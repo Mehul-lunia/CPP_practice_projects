e0f422e539d5c3b3828e7d6bcd32eb44a0df29d2
Mehul-lunia <mehulrajlunia@gmail.com>
2024-03-28 21:41:54 +0530

    Deleted a few useless files

--- window_hello_world.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <windows.h>
-// Displays a Window showing the text Hello World!
-int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
-{
-    return MessageBox(NULL,"Hello World!","My first windowed program",MB_ICONASTERISK);
-}
